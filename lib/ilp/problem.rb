341a87be19524603c018538133f8c5d44dc91940
Guillaume Verger <verger.guillaume@gmail.com>
2015-10-22 19:16:52 +0200

    Indentations problems

--- lib/ilp/problem.rb
+++ lib/ilp/problem.rb
@@ -12,60 +12,60 @@ module Ilp
       @problem = Rglpk::Problem.new
 
       unless vars.empty?
-      problem.add_cols(vars.length)
-      current_col_idx = 0
-      @column_idx = Hash.new
-      vars.each do |var|
-        col = problem.cols[current_col_idx]
-        @column_idx[var] = col.j - 1
-        col.name = var.name unless var.name.nil?
-        col.kind = var.kind
-        if var.kind == Rglpk::GLP_BV
-          var.bounds = 0..1
-        end
-        if var.upper_bound.nil?
-          if var.lower_bound.nil?
-            col.set_bounds(Rglpk::GLP_FR, nil, nil)
-          else
-            col.set_bounds(Rglpk::GLP_LO, var.lower_bound, nil)
+        problem.add_cols(vars.length)
+        current_col_idx = 0
+        @column_idx = Hash.new
+        vars.each do |var|
+          col = problem.cols[current_col_idx]
+          @column_idx[var] = col.j - 1
+          col.name = var.name unless var.name.nil?
+          col.kind = var.kind
+          if var.kind == Rglpk::GLP_BV
+            var.bounds = 0..1
           end
-        else
-          if var.lower_bound.nil?
-            col.set_bounds(Rglpk::GLP_UP, nil, var.upper_bound)
-          else
-            if var.lower_bound != var.upper_bound
-              col.set_bounds(Rglpk::GLP_DB, var.lower_bound, var.upper_bound)
+          if var.upper_bound.nil?
+            if var.lower_bound.nil?
+              col.set_bounds(Rglpk::GLP_FR, nil, nil)
             else
-              col.set_bounds(Rglpk::GLP_FX, var.lower_bound, var.upper_bound)
+              col.set_bounds(Rglpk::GLP_LO, var.lower_bound, nil)
+            end
+          else
+            if var.lower_bound.nil?
+              col.set_bounds(Rglpk::GLP_UP, nil, var.upper_bound)
+            else
+              if var.lower_bound != var.upper_bound
+                col.set_bounds(Rglpk::GLP_DB, var.lower_bound, var.upper_bound)
+              else
+                col.set_bounds(Rglpk::GLP_FX, var.lower_bound, var.upper_bound)
+              end
             end
           end
+          current_col_idx += 1
         end
-        current_col_idx += 1
-      end
       end
 
       unless constraints.empty?
-      problem.add_rows(constraints.length)
-      current_row_idx = 0
-      constraints.each do |constraint|
-        coefs = Array.new(problem.cols.size, 0)
-        constraint.terms.each do |term|
-          coefs[@column_idx[term.var]] += term.mult
+        problem.add_rows(constraints.length)
+        current_row_idx = 0
+        constraints.each do |constraint|
+          coefs = Array.new(problem.cols.size, 0)
+          constraint.terms.each do |term|
+            coefs[@column_idx[term.var]] += term.mult
+          end
+          row = problem.rows[current_row_idx]
+          row.set(coefs)
+          row.set_bounds(constraint.type, constraint.bound, constraint.bound)
+          current_row_idx += 1
         end
-        row = problem.rows[current_row_idx]
-        row.set(coefs)
-        row.set_bounds(constraint.type, constraint.bound, constraint.bound)
-        current_row_idx += 1
-      end
       end
 
       unless objective.nil?
-      problem.obj.dir = objective.objective_function
-      coefs = Array.new(problem.cols.size, 0)
-      objective.terms.each do |term|
-        coefs[@column_idx[term.var]] = term.mult
-      end
-      problem.obj.coefs = coefs
+        problem.obj.dir = objective.objective_function
+        coefs = Array.new(problem.cols.size, 0)
+        objective.terms.each do |term|
+          coefs[@column_idx[term.var]] = term.mult
+        end
+        problem.obj.coefs = coefs
       end
     end
 
@@ -97,7 +97,7 @@ module Ilp
 
     def obj_value
       if @sol_type == :mip
-      Glpk_wrapper.glp_mip_obj_val(problem.lp)
+        Glpk_wrapper.glp_mip_obj_val(problem.lp)
       else
         Glpk_wrapper.glp_get_obj_val(problem.lp)
       end
